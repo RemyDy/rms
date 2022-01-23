4c3ed0d63b4d1b3f5035ed42837f2869ebddf763
RemyDy <regcschut@gmail.com>
2022-01-23 20:43:09 +0100

    homepage api request functie gemaakt

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import "./HomePage.css"
+import Tile from "../../components/tiles/Tile";
 
-function HomePage(){
-    return(
+
+// Api Key: 509cee8e
+
+
+function HomePage() {
+    const [movieQuote, setMovieQuote] = useState("You had me at Hello")
+    const [movie, setMovie] = useState({});
+    console.log(movie);
+
+    async function fetchMovieData() {
+        try {
+            const result = await axios.get("https://www.omdbapi.com/?i=tt3896198&apikey=509cee8e", {});
+            console.log(result);
+            setMovie(result.data);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    return (
         <>
-        <div>HomePage</div>
+            <Tile />
+
+            <section className="tile">
+                <button
+                    type="button"
+                    onClick={() => fetchMovieData() + setMovieQuote("To infinity and beyond")}
+                >
+                    Find
+                </button>
+            </section>
+            <section>
+                <div>
+                    <p>
+                        {movieQuote}
+                    </p>
+                </div>
+                <div>
+                    <h4>Title</h4>
+                    <p>{movie.Title}</p>
+                    <h4>Director</h4>
+                    <p>{movie.Director}</p>
+                    <h4>Genre</h4>
+                    <p>{movie.Genre}</p>
+                    <h4>Released</h4>
+                    <p>{movie.Released}</p>
+                    <h4>IMDB Rating</h4>
+                    <p>{movie.imdbRating}</p>
+                </div>
+                <div>
+                    <h4>Plot</h4>
+                    <p>{movie.Plot}</p>
+                </div>
+            </section>
         </>
     )
 }
--- src/pages/searchmoviepage/SearchMoviePage.js
+++ src/pages/searchmoviepage/SearchMoviePage.js
@@ -1,8 +1,36 @@
 import React from "react";
+import {useForm} from "react-hook-form";
 
-function SearchMoviePage(){
-    return(
-        <div>SearchMoviePage</div>
+function SearchMoviePage() {
+    return (
+        <>
+            <div>SearchMoviePage</div>
+            <section>
+                <form>
+                        <label>
+                            <input/>
+                        </label>
+                        <label>
+                            <input/>
+                        </label>
+                        <label>
+                            <input/>
+                        </label>
+                    <button>
+                        Find
+                    </button>
+                </form>
+            </section>
+
+            <section>
+                <div> Title:
+                    <p>[Title]</p>
+                </div>
+                <div> Year:
+                    <p>[Year]</p>
+                </div>
+            </section>
+        </>
     )
 }
 
